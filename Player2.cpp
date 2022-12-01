9e2139fe538bc8b8659df7f8b6e5b5bf53b23175
Alberto Pesqueda <106642220+Bones216@users.noreply.github.com>
2022-12-01 23:45:53 +0000

    Added Player2 cpp and h

---
+++ Player2.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "Player2.h"
+using namespace std;
+
---
+++ Player2.h
@@ -0,0 +1,16 @@
+#ifndef Player2_hpp
+#define Player2_hpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Player2 
+{
+private:
+
+public:
+	
+
+};
+
+#endif /* student_hpp */
